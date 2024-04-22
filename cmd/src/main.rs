97549b12cf5c87bed0d5cda6be5ce9a0c6329fe7
D-lyw <liuyuanwang4321@gmail.com>
2024-04-22 23:31:51 +0800

    feat: add cli command generate password

--- cmd/src/main.rs
+++ cmd/src/main.rs
@@ -1,17 +1,51 @@
-use clap::{App, Arg, SubCommand};
+mod password_generate;
+use clap::{value_parser, Parser, Subcommand};
+
+use password_generate::password_generate;
+
+#[derive(Parser, Debug)]
+#[command(version, about = "some command line utilities")]
+struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    // password generate
+    #[command(name = "pass-generate", about = "generate password")]
+    PassGenerate(PassGenerateOption),
+}
+
+#[derive(Parser, Debug)]
+struct PassGenerateOption {
+    #[arg(short, long, default_value_t = 16, value_parser = value_parser!(u8).range(6..))]
+    length: u8,
+    #[arg(long, default_value_t = true)]
+    number: bool,
+    #[arg(long, default_value_t = true)]
+    lowercase: bool,
+    #[arg(long, default_value_t = true)]
+    symbol: bool,
+    #[arg(long, default_value_t = true)]
+    uppercase: bool,
+}
 
 fn main() {
-    let matches = App::new("cmd").
-        version("v0.1.0")
-        .about("Test cmd program")
-        .arg(Arg::with_name("file")
-            .short("f")
-            .long("file")
-            .value_name("FILE")
-            .help("input file path")
-            .takes_value(true).required(true)
-        ).subcommand(SubCommand::with_name("help").about("subcommand help"))
-        .get_matches();
-    let path = matches.value_of("FILE").unwrap();
-    println!("{}", path);
+    let cli = Args::parse();
+
+    match &cli.command {
+        Some(Commands::PassGenerate(options)) => {
+            password_generate(
+                options.length,
+                options.number,
+                options.lowercase,
+                options.symbol,
+                options.uppercase,
+            );
+        }
+        _ => {
+            println!("other");
+        }
+    }
 }
---
+++ cmd/src/password_generate.rs
@@ -0,0 +1,39 @@
+use rand::seq::SliceRandom;
+use rand::{thread_rng, Rng};
+
+pub fn password_generate(length: u8, number: bool, uppercase: bool, lowercase: bool, symbol: bool) {
+    let mut password = Vec::new();
+    let mut chars = Vec::new();
+    if number {
+        let number_chars = vec!['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        password.push(number_chars[thread_rng().gen_range(0..number_chars.len())]);
+        chars.extend(number_chars);
+    }
+    if lowercase {
+        let lowercase_chars = vec![
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q', 'r', 's', 't',
+            'u', 'v', 'w', 'x', 'y', 'z',
+        ];
+        password.push(lowercase_chars[thread_rng().gen_range(0..lowercase_chars.len())]);
+        chars.extend(lowercase_chars);
+    }
+    if uppercase {
+        let uppercase_chars = vec![
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z',
+        ];
+        password.push(uppercase_chars[thread_rng().gen_range(0..uppercase_chars.len())]);
+        chars.extend(uppercase_chars);
+    }
+    if symbol {
+        let symbol_chars = vec!['@', '#', '$', '%', '&', '*', '_', '-', '+', '='];
+        password.push(symbol_chars[thread_rng().gen_range(0..symbol_chars.len())]);
+        chars.extend(symbol_chars);
+    }
+
+    for _ in 0..(length - password.len() as u8) {
+        password.push(chars[thread_rng().gen_range(0..chars.len())]);
+    }
+    password.shuffle(&mut thread_rng());
+    println!("{}", String::from_iter(password));
+}
